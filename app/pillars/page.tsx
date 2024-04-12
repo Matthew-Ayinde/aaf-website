3fdd929ba85eb67a61dddce18fa68c441a29ba0a
Matthew Ayinde <ayindematthew2003@gmail.com>
2024-04-13 00:05:35 +0100

    pillar branch

---
+++ app/pillars/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PillarComponent from '@/components/PillarComponent'
+
+const pillar = () => {
+  return(
+    <div>
+      <PillarComponent />
+    </div>
+  )
+}
+
+
+export default pillar
