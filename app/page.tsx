82654418e433ea00d70bf4cfcc0f411a5e782500
lawal069 <lawaloluwaseyi069@gmail.com>
2024-04-14 11:12:00 +0100

    Info Box

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import AboutUsPage from "@/components/AboutUsPage";
-import LearnMorepage from "@/components/LearnMorepage";
-
 export default function Home() {
 
   return (
     <div className="w-full mx-auto">
-      {/* Home Page */}
-      {/* <AboutUsPage /> */}
-      <LearnMorepage />
+      
     </div>
   );
 }
