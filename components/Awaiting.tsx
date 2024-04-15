db7f8b9da5e792137717ac9649853d549ba2938c
lawal069 <lawaloluwaseyi069@gmail.com>
2024-04-15 03:14:10 -0700

    all branches

--- components/Awaiting.tsx
+++ components/Awaiting.tsx
@@ -11,11 +11,11 @@ const Awaiting = () => {
     <div className="w-full mx-auto bg-custom-dark-bg lg:px-20 px-6 lg:py-[120px] py-[100px]">
       <div className="lg:flex lg:flex-col-reverse">
         <div className="flex flex-col lg:flex-row lg:gap-20 gap-14 items-center justify-center">
-          <div>
-            <ImageComp img1={img1} img2={img2} img3={img3} />
-          </div>
-          <div className="flex flex-col gap-4">
-            <h1 className="font-bold lg:text-5xl text-2xl text-custom-white">Awaiting Content</h1>
+          <ImageComp img1={img1} img2={img2} img3={img3} />
+          <div className="flex flex-col gap-4 w-full lg:w-[479px]">
+            <h1 className="font-bold lg:text-5xl text-2xl text-custom-white">
+              Awaiting Content
+            </h1>
             <p className="font-normal lg:text-lg text-base text-custom-tertiary-text">
               Advancing Lives  - Providing and promoting improved welfare for
               the vulnerable; Improving care, excellence and enterprise in
